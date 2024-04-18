cc40d2b4e20725c34e1869059fe22f911a3b855f
merve <merve.gormus@dt.net.tr>
2024-04-18 12:59:18 +0300

    fix ui

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,31 +1,43 @@
 import React from 'react';
 import '../styles.css';
-
-import {useNavigate} from 'react-router-dom'
-import {Products} from "../utils/types";
 import {useSelector} from "react-redux";
 import {selectMovieDetail} from "../store/actions/movieSlice";
 
-
 const MovieCard: React.FC = () => {
     const product = useSelector(selectMovieDetail)
-//todo:Ratings
+
     return (
-        <div className="w-a  bg-white p-2  shadow-md">
-            {product && <div className="h-full">
-                <img src={product?.Poster} alt="Image" className="max-w-full  "/>
-                <div className="text-black text-left mb-4 mt-4">
-                    <p className="text-blue-500 mb-2">{product.Title}</p>
-                    <p>{product.Year}</p>
-                    <p>{product.Genre}</p>
-                    <p>{product.Released}</p>
-                    <p>{product.Director}</p>
-                    <p>{product.Writer}</p>
-                    <p>{product.Actors}</p>
-                    <p>{product.Runtime}</p>
-                </div>
-            </div>}
-        </div>
+            <div className=" bg-white p-4 shadow-md  ">
+                {product && (
+                    <div className="flex items-center w-a">
+                        {product.Poster &&  <div className="w-1/3">
+                            <img src={product.Poster} alt="Image" className="max-w-full"/>
+                        </div>
+                        }
+                        <div className="w-2/3 ml-4">
+                            <p className="text-blue-500 mb-2">{product.Title}</p>
+                            <p><span className="font-semibold">Year:</span> {product.Year}</p>
+                            <p><span className="font-semibold">Genre:</span> {product.Genre}</p>
+                            <p><span className="font-semibold">Released:</span> {product.Released}</p>
+                            <p><span className="font-semibold">Director:</span> {product.Director}</p>
+                            <p><span className="font-semibold">Writer:</span> {product.Writer}</p>
+                            <p><span className="font-semibold">Actors:</span> {product.Actors}</p>
+                            <p><span className="font-semibold">Runtime:</span> {product.Runtime}</p>
+                            {product.Ratings && product.Ratings.length > 0 && (
+                                <div>
+                                    <p className="font-semibold">Ratings:</p>
+                                    <ul>
+                                        {product.Ratings.map((rating: { Source:string, Value:string }, index: number) => (
+                                            <li key={index}>{rating.Source}: {rating.Value}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            )}
+                        </div>
+                    </div>
+                )}
+            </div>
+
     );
 };
 
--- src/pages/MoviesList.tsx
+++ src/pages/MoviesList.tsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {getMovies, selectMovies} from "../store/actions/movieSlice";
+import {getMovieDetail, getMovies, selectMovies} from "../store/actions/movieSlice";
 import {Products} from "../utils/types";
 import {AppDispatch} from "../store";
 import {useNavigate} from "react-router-dom";
+import '../styles.css';
 
 const MoviesList: React.FC = () => {
     const dispatch: AppDispatch = useDispatch()
@@ -12,14 +13,16 @@ const MoviesList: React.FC = () => {
     const [filteredData, setFilteredData] = useState<Products[]>(Search)
     const [search, setSearch] = useState('')
     const [year, setYear] = useState('')
-    const [currentPage, setCurrentPage] = useState<number>()
+    const [currentPage, setCurrentPage] = useState<number>(1)
     const totalPages = +totalResults / 10
     const mappingData = filteredData?.length > 0 ? filteredData : (Search || [])
 
+    const [type, setType] = useState('movie');
 
-    /*● Necessary commands to run the application should preferably be communicated in the
-    README.md file.
-    */
+    const handleTypeChange = (newType: string) => {
+        setType(newType);
+        dispatch(getMovies({type: newType, page: currentPage, y: year}));
+    };
 
     useEffect(() => {
         if (Search && Search.some((item: Products) => item.Title.toLowerCase().includes(search.toLowerCase()))) {
@@ -28,56 +31,49 @@ const MoviesList: React.FC = () => {
         } else {
             setFilteredData([])
         }
-    }, [search])
+    }, [search, Search])
 
     return (
-        <div className="flex flex-wrap gap-6 align-middle">
-            <div className="flex items-center border rounded p-2">
-                <span className="mr-2">🔍</span>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    className="flex-grow border-none focus:outline-none"
-                    onChange={(e) => {
-                        setSearch(e.target.value)
-                    }}
-                    value={search}
-                    data-id={'search-input'}
-                />
-            </div>
-            <div className="flex items-center border rounded p-2">
-                <span className="mr-2">📅</span>
-                <input
-                    type="text"
-                    placeholder="Year..."
-                    className="flex-grow border-none focus:outline-none"
-                    onChange={(e) => {
-                        setYear(e.target.value)
-                        dispatch(getMovies({y: e.target.value, type: 'movie'}))
-                    }}
-                    value={year}
-                    data-id={'search-input'}
-                />
+        <div className="flex flex-wrap gap-6 align-middle flex-col p-3">
+            <div className={"flex flex-wrap gap-6 align-middle flex-row"}>
+                <div className="flex items-center border rounded p-2">
+                    <span className="mr-2">🔍</span>
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className="flex-grow border-none focus:outline-none"
+                        onChange={(e) => {
+                            setSearch(e.target.value)
+                        }}
+                        value={search}
+                        data-id={'search-input'}
+                    />
+                </div>
+                <div className="flex items-center border rounded p-2">
+                    <span className="mr-2">📅</span>
+                    <input
+                        type="text"
+                        placeholder="Year..."
+                        className="flex-grow border-none focus:outline-none"
+                        onChange={(e) => {
+                            setYear(e.target.value)
+                            dispatch(getMovies({y: e.target.value, type: type, page: currentPage}))
+                        }}
+                        value={year}
+                        data-id={'search-input'}
+                    />
+                </div>
             </div>
             <div className="flex gap-4 mt-4">
-                <button
-                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
-                    onClick={() => dispatch(getMovies({type: 'movie'}))}
-                >
-                    Movies
-                </button>
-                <button
-                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
-                    onClick={() => dispatch(getMovies({type: 'series'}))}
-                >
-                    Series
-                </button>
-                <button
-                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors"
-                    onClick={() => dispatch(getMovies({type: 'episode'}))}
-                >
-                    Episodes
-                </button>
+                {['movie', 'series', 'episode'].map((buttonType) => (
+                    <button
+                        key={buttonType}
+                        className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors ${type === buttonType ? 'bg-blue-600' : ''}`}
+                        onClick={() => handleTypeChange(buttonType)}
+                    >
+                        {buttonType.charAt(0).toUpperCase() + buttonType.slice(1)}
+                    </button>
+                ))}
             </div>
             <div className="overflow-x-auto">
                 <table className="table-auto w-full border-collapse border border-gray-400 text-left">
@@ -92,7 +88,7 @@ const MoviesList: React.FC = () => {
                     <tbody>
                     {mappingData.map((product: Products) => (
                         <tr className="cursor-pointer" key={product.imdbID} onClick={() => {
-                            dispatch(getProductDetail(product.imdbID))
+                            dispatch(getMovieDetail(product.imdbID))
                             navigate(`detail/${product.imdbID}`)
                         }
                         }>
@@ -111,7 +107,7 @@ const MoviesList: React.FC = () => {
                             className={`px-3 py-1 border rounded ${currentPage === page ? 'bg-blue-500 text-white' : 'text-blue-500 hover:bg-blue-100'}`}
                             onClick={() => {
                                 setCurrentPage(page)
-                                dispatch(getMovies({page: page, type: 'movie'}))
+                                dispatch(getMovies({page: page, type: type, y: year}))
                                 setSearch('')
                             }}
                         >
